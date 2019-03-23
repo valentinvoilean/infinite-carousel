f08983283939a603457afabc4403df1624df522d
valentin <vali.voilean@gmail.com>
2019-03-23 16:32:33 +0100

    install cool-images; pass 10 images to the gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import 'normalize.css';
 import 'react-placeholder/lib/reactPlaceholder.css';
 
+// @ts-ignore
+import coolImages from 'cool-images';
 import React from 'react';
 import GoogleFontLoader from 'react-google-font-loader';
 import { ThemeProvider } from 'styled-components';
@@ -11,13 +13,21 @@ import { StyledApp, StyledLink, StyledTitle } from './styled';
 import { fonts } from './constants';
 import { theme } from './theme';
 
-export const App: React.FunctionComponent = () => (
-  <ThemeProvider theme={theme}>
-    <StyledApp>
-      <GoogleFontLoader fonts={fonts} />
-      <StyledTitle>Infinite Carousel</StyledTitle>
-      <Carousel />
-      <StyledLink href="http://valentinvoilean.com">by Valentin-Marian Voilean</StyledLink>
-    </StyledApp>
-  </ThemeProvider>
-);
+export const App: React.FunctionComponent = () => {
+  const images = coolImages.many(576, 1024, 10);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StyledApp>
+        <GoogleFontLoader fonts={fonts} />
+        <StyledTitle>Infinite Carousel</StyledTitle>
+        <Carousel>
+          {images.map((url: string, index: number) => (
+            <img src={url} key={index} alt={`img-${index}`} />
+          ))}
+        </Carousel>
+        <StyledLink href="http://valentinvoilean.com">by Valentin-Marian Voilean</StyledLink>
+      </StyledApp>
+    </ThemeProvider>
+  );
+};
